ff027a9e5cd9e26ae341a2a54609f5da2a987a21
OppositeIllusion <45372082+OppositeIllusion@users.noreply.github.com>
2019-01-30 20:17:52 -0600

    WIP

--- AbstractFactory/IDumb.cs
+++ AbstractFactory/IDumb.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Author: Nicholas Pica
+//FileName: IDumb.cs
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace AbstractFactory
 {
     abstract class IDumb
     {
+        public String getName;
     }
 }
